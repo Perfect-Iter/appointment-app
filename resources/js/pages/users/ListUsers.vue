e9c64f6ef29bc929a1bb034a69bd54a907cdbfe9
ruben <barazaruben.br@gmail.com>
2023-05-30 10:12:48 +0300

    initial commit

---
+++ resources/js/pages/users/ListUsers.vue
@@ -0,0 +1,138 @@
+<template>
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Users</h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="/admin/dashboard">Home</a></li>
+                <li class="breadcrumb-item active">Users</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="content">
+        <div class="container-fluid">
+
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createUserModal">
+                Add New User
+            </button>
+
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <thead>
+                            <th style="width: 10px;">#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Registered</th>
+                            <th>Role</th>
+                            <th>Options</th>
+                        </thead>
+                        <tbody>
+                            <tr v-for="(user, index) in users" :key="user.id">
+                                <td>{{ index + 1 }}</td>
+                                <td>{{user.name}}</td>
+                                <td>{{user.email}}</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+      </div>
+
+<!-- Modal -->
+<div class="modal fade" id="createUserModal" data-backdrop="static" tabindex="-1" role="dialog"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Add New User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <Form @submit = "createUser">
+                    <div class="modal-body">
+
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <Field name="name" type="text" class="form-control " id="name"
+                                    aria-describedby="nameHelp" placeholder="Enter full name" />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <Field name="email" type="email" class="form-control " id="email"
+                                    aria-describedby="nameHelp" placeholder="Enter full name" />
+                            </div>
+
+
+                        <div class="form-group">
+                            <label for="email">Password</label>
+                            <Field type="password" name="password" class="form-control " id="password"
+                                aria-describedby="nameHelp" placeholder="Enter password" />
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="submit"  class="btn btn-primary">Save</button>
+                    </div>
+                </Form>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+    import axios from 'axios'
+    import { onMounted, reactive, ref } from 'vue'
+    import {Form, Field} from 'vee-validate'
+
+    const users = ref([])
+    const form = reactive({
+        name: '',
+        email: '',
+        password: '',
+    })
+
+    const getUsers = () =>{
+        axios.get('/api/users')
+        .then((res) =>{
+            users.value = res.data
+        })
+    }
+/*     const createUser = ()=>{
+        axios.post('/api/users/add/', form)
+        .then((res) =>{
+            users.value.push(res.data)
+            form.name = '';
+            form.email = '';
+            form.password = '';
+            $('#createUserModal').modal('hide');
+        })
+    }
+*/
+
+    const createUser = (values) => {
+        console.log(values)
+    }
+
+
+    onMounted(()=> {
+        getUsers();
+    })
+</script>
+
+<style>
+
+</style>
